BarbeRousseLibre/hermessenger

commit 8b65e2673ac3f5ed1a2d84581661366958fab2fb

Adding several checks for the case where no mails has to be send, exit the script instead of throwing useless error about it. Avoiding also to try to send the .gitkeep file.

src/functions.php
@@ -80,11 +80,13 @@ function store_sended_mail_to_logs($pending_mail_path, $status, $locations) {
 
 }
 
-function return_oldest_mail($temp_mail_dir) {
+function return_oldest_mail($temp_mail_dir, $exclude=".gitkeep") {
     /*
      * From the path given as argument, return only the oldest file, using scandir.
      *
      * $temp_mail_dir should be the location of the holding queue directory for mail.
+     *
+     * $exclude is a string reprensentating the .gitkeep file used to allow to pull this directory, empty.
      *
      * Return an array with all data's from the mail.
      *
@@ -96,8 +98,18 @@ function return_oldest_mail($temp_mail_dir) {
     // Retrieve all files in the temp directory as an array
     $files = scandir($temp_mail_dir);
 
-    // scandir() return per default the oldest file as first in list, after '.' and '..'. Index being 2 for the first.
-    $oldest_mail = $temp_mail_dir . $files[2];
+    // Check if an $exclude file is present, changes the index of the oldest mail accordingly
+    ($files[2] == $exclude ? $mail_index = 3 : $mail_index = 2);
+
+    if (!array_key_exists($mail_index, $files)) { // If no mail to send
+
+        exit;
+
+    } else { // There is a mail to send
+
+        $oldest_mail = $files[$mail_index];
+
+    }
 
     // Retrieve in the file the desired field, delimited by '|'
     $mail_file = @fopen($oldest_mail, 'r');

commit 0c82639d75e52b9af9fd594fb49404c9457f6922

Fixing bug introduced in 8b65e2673ac3f5ed1a2d84581661366958fab2fb precedent commit, the path was not added to the name's file.

src/functions.php
@@ -99,7 +99,7 @@ function return_oldest_mail($temp_mail_dir, $exclude=".gitkeep") {
     $files = scandir($temp_mail_dir);
 
     // Check if an $exclude file is present, changes the index of the oldest mail accordingly
-    ($files[2] == $exclude ? $mail_index = 3 : $mail_index = 2);
+    ($files[2] === $exclude ? $mail_index = 3 : $mail_index = 2);
 
     if (!array_key_exists($mail_index, $files)) { // If no mail to send
 
@@ -107,7 +107,7 @@ function return_oldest_mail($temp_mail_dir, $exclude=".gitkeep") {
 
     } else { // There is a mail to send
 
-        $oldest_mail = $files[$mail_index];
+        $oldest_mail = $temp_mail_dir . $files[$mail_index];
 
     }
 