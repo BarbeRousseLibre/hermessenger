fcbce61ceec82cf08c43b1b0aae0c827d2fdf38f
Kevin GASPARD DE RENEFORT <misc@koshie.fr>
2025-02-06 14:09:48 +0100

    Forgot to add edition for main.php

--- public/main.php
+++ public/main.php
@@ -8,21 +8,16 @@
  * The web mail form should target this file to redirect the user's input data to Hermessenger:
  * ' <form action="/main.php" method="post"> '
  *
- * This file should be accessible to your web server user and only this file (from Hermessenger).
+ * This file should be accessible to your web server user.
  *
  */
-
-require __DIR__ . '/../vendor/barberousselibre/hermessenger/config/server_path_config.php';
 
 // Load composer
 require __DIR__ . '/../vendor/autoload.php';
 
-/*
- * Pre-copy $_POST to $raw_post_copy & avoid to works on the real datas, then is updated in the script once it was
- * cleaned for further testing.
- *
- */
+/* Pre-copy $_POST to $raw_post_copy & avoid to works on the real datas, then it is updated in the script once it was
+ * cleaned for further testing. */
 $raw_post_copy = $_POST;
 
-// Redirect user's input for testing it
-require_once __DIR__ . $hermessenger_source . 'src/checking_form.php';
+// Redirect user's input to test it
+require_once __DIR__ . '/../vendor/barberousselibre/hermessenger/src/checking_form.php';
